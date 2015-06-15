5bb994571aa656d43b893e62fb6337052b464c03
Kamlesh Korat <koratkamlesh@gmail.com>
2015-06-15 16:52:53 -0400

    Update from 15-June 2015

---
+++ WicketPractice/src/main/java/com/product/Page1.java
@@ -0,0 +1,12 @@
+package com.product;
+
+import org.apache.wicket.markup.html.WebPage;
+
+/**
+ * Created by kkorat on 6/15/2015.
+ */
+public class Page1 extends WebPage {
+    public Page1(){
+
+    }
+}
